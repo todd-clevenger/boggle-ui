c74b4ba2937eb4bb2ca24a08734f9c80e9de7672
Todd Clevenger <todcle@hotmail.com>
2019-03-29 14:13:57 -0700

    Adds Board Initialization

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './Board.css';
+//import { connectableObservableDescriptor } from 'rxjs/internal/observable/ConnectableObservable';
 
 function Tile(props) {
   return (
-    <div class="boggle">
+    <div className="boggle">
       <span>{props.value}</span>
     </div>
   );
@@ -14,37 +15,55 @@ export default class Board extends React.Component {
     super(props);
 
     this.state = {
-      tiles: ["U","I","S","L","D","U","E","M","T","B","Y","L","Y","I","R","D"],
+      tiles: ["","","","","","","","","","","","","","","",""],
     }
+
+    // TODO:  Clean up async initialization - need a prestate while the
+    // remote call & promise complete.  This method loads with a default &
+    // then updates after the fact.
+    this.getBoard();
   }
 
+  getBoard() {
+    fetch('http://localhost:3000/v1/board', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then(res => res.json())
+    .then(json => this.setState({tiles: json.tiles}))
+    .then(data => console.log(JSON.stringify(data)))
+    .catch(error => console.error('Error:', error));
+  }
+  
   renderTile(i) {
     return (
       <Tile value={this.state.tiles[i]} />
     )
   }
+
   render() {
     return (
       <div id="board">
-        <div class="row">
+        <div className="row">
           {this.renderTile(0)}
           {this.renderTile(1)}
           {this.renderTile(2)}
           {this.renderTile(3)}
         </div>
-        <div class="row">
+        <div className="row">
           {this.renderTile(4)}
           {this.renderTile(5)}
           {this.renderTile(6)}
           {this.renderTile(7)}
         </div>
-        <div class="row">
+        <div className="row">
           {this.renderTile(8)}
           {this.renderTile(9)}
           {this.renderTile(10)}
           {this.renderTile(11)}
         </div>
-        <div class="row">
+        <div className="row">
           {this.renderTile(12)}
           {this.renderTile(13)}
           {this.renderTile(14)}
